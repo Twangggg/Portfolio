55a143088182dbffc6a583c7fd3be35de0bdc06e
Twangggg <tktoan10a1@gmail.com>
2026-05-22 12:52:50 +0700

    fix: Comment out unused iframe for 3D background in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import ProjectDetailPage from "./pages/ProjectDetailPage.jsx";
 function App() {
   return (
     <div className="relative h-screen overflow-hidden">
-      <iframe
+      {/* <iframe
         src="https://my.spline.design/nexbotrobotcharacterconceptforpersonaluse-RAtYzrKI8rT6HVspQbsJBH05/"
         className="fixed inset-0 z-0 w-full h-full"
         style={{ border: "none" }}
         title="3D background"
         aria-hidden="true"
         scrolling="no"
-      />
+      /> */}
       <div className="relative z-10 h-full pointer-events-none">
         <Routes>
           <Route path="/" element={<HomePage />} />
